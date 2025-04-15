152d908739fc5a8a9b6a3eb60049e0e0d9d37805
vheedev <vhee.dev@gmail.com>
2025-04-16 02:39:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,15 +14,24 @@ st.set_page_config(
 # --- Fintari Header with Logo and Mode Selector ---
 header_col1, header_col2, header_col3 = st.columns([1, 4, 2])
 
-with header_col1:
-    st.image("logo.png", width=80)
+st.markdown(
+        "<div style='display: flex; align-items: center; height: 80px;'>"
+        "<img src='logo.png' width='50' style='margin-right: 10px;'/>"
+        "</div>",
+        unsafe_allow_html=True,
+    )
 
-with header_col2:
-    st.markdown("<h1 style='margin-top: 12px;'>Fintari</h1>", unsafe_allow_html=True)
+with col2:
+    st.markdown(
+        "<div style='display: flex; align-items: center; height: 60px;'>"
+        "<h1 style='margin: 0;'>Fintari</h1>"
+        "</div>",
+        unsafe_allow_html=True,
+    )
 
-with header_col3:
-    mode = st.selectbox(" ", ["Login", "Register"], label_visibility="collapsed", index=0)
-
+with col3:
+    st.markdown("<div style='height: 18px;'></div>", unsafe_allow_html=True)  # spacer for alignment
+    mode = st.selectbox("", ["Login", "Register"], label_visibility="collapsed")
 
 # --- Session State Setup ---
 if "logged_in" not in st.session_state:
