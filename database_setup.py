5d138ad684ea3bae39d76e37b6587212179442d5
vheedev <vhee.dev@gmail.com>
2025-04-16 03:03:37 +0700

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -12,13 +12,18 @@ def create_tables():
     conn = sqlite3.connect("finance_app.db")
     cursor = conn.cursor()
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT UNIQUE NOT NULL,
-            password TEXT NOT NULL
+   cursor.execute("""
+    CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT NOT NULL,
+        amount REAL NOT NULL,
+        description TEXT,
+        type TEXT CHECK(type IN ('Income', 'Expense')) NOT NULL,
+        account TEXT,
+        username TEXT NOT NULL
         )
     """)
+
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS transactions (
